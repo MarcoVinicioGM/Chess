c0fe8dbced044384823669df7db64b4096b5b5d6
Marco Garcia <mgarc84@lsu.edu>
2024-04-27 07:09:39 -0500

    FIXED CHECKFORGAMEOVER FUNCTION AAHHH This is the best version we have before adding StartMenu and Atomic Chess

--- Chesslogic/Game.cs
+++ Chesslogic/Game.cs
@@ -26,6 +26,38 @@ namespace Chesslogic
         {
             move.Execute(Board);
             Current = Current.Oppponent();
+<<<<<<< HEAD
+=======
+            CheckForGameOver();
+            System.Diagnostics.Debug.WriteLine(Result);
+        }
+        public IEnumerable<Moves> AllPossibleMoves(Player player)
+        {
+            IEnumerable<Moves> moveCandidates = Board.PiecePositionsFor(player).SelectMany(pos =>
+            {
+                Piece piece = Board[pos];
+                return piece.GetMoves(pos, Board);
+            });
+            return moveCandidates.Where(move => move.isLegal(Board));
+        }
+        public void CheckForGameOver()
+        {
+            if (!AllPossibleMoves(Current).Any())
+            {
+                if (Board.IsInCheck(Current))
+                {
+                    Result = Result.Win(Current.Oppponent());
+                }
+                else
+                {
+                    Result = Result.Draw(Endreason.Stalemate);
+                }
+            }
+        }
+        public bool IsGameOver()
+        {
+            return Result != null;
+>>>>>>> e4ac12f (FIXED CHECKFORGAMEOVER FUNCTION AAHHH)
         }
     }
 }
