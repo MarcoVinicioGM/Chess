43d76657e2349b8fb36fc4b10d4a0dff0735b60f
Marco Garcia <mgarc84@lsu.edu>
2024-05-09 22:46:57 -0500

    Final Push to Dev deleted unnecessary lines and files.

--- ChessUI/PromotionMenu.xaml.cs
+++ ChessUI/PromotionMenu.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows.Controls;
 
 namespace ChessUI
 {
-    /// <summary>
-    /// Interaction logic for PromotionMenu.xaml
-    /// </summary>
     public partial class PromotionMenu : UserControl
     {
         public event Action<PieceType> PieceSelected;
@@ -14,27 +11,25 @@ namespace ChessUI
         {
             InitializeComponent();
             QueenW.Source = Images.GetImage(player, PieceType.Queen);
-            RookW.Source = Images.GetImage(player, PieceType.Rook);
             BishopW.Source = Images.GetImage(player, PieceType.Bishop);
+            RookW.Source = Images.GetImage(player, PieceType.Rook);
             KnightW.Source = Images.GetImage(player, PieceType.Knight);
         }
         private void Queen_MouseDown(object sender, RoutedEventArgs e)
         {
             PieceSelected?.Invoke(PieceType.Queen);
-
-        }
-        private void Rook_MouseDown(object sender, RoutedEventArgs e)
-        {
-            PieceSelected?.Invoke(PieceType.Rook);
         }
         private void Bishop_MouseDown(object sender, RoutedEventArgs e)
         {
             PieceSelected?.Invoke(PieceType.Bishop);
         }
+        private void Rook_MouseDown(object sender, RoutedEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Rook);
+        }
         private void Knight_MouseDown(object sender, RoutedEventArgs e)
         {
             PieceSelected?.Invoke(PieceType.Knight);
         }
-
     }
 }
